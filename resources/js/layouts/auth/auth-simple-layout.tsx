01261afdeecc1a351ec3838939b2f7f6044150a8
Oliveira Anna <peertopeer700@gmail.com>
2026-04-30 11:13:49 -0300

    style: add logo in auth template

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -17,18 +17,9 @@ export default function AuthSimpleLayout({
                             href={home()}
                             className="flex flex-col items-center gap-2 font-medium"
                         >
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
-                            </div>
+                            <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
                             <span className="sr-only">{title}</span>
                         </Link>
-
-                        <div className="space-y-2 text-center">
-                            <h1 className="text-xl font-medium">{title}</h1>
-                            <p className="text-center text-sm text-muted-foreground">
-                                {description}
-                            </p>
-                        </div>
                     </div>
                     {children}
                 </div>
