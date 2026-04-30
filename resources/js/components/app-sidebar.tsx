f5034f972ddf0feae2da88b8300d05d57fca1cff
Oliveira Anna <peertopeer700@gmail.com>
2026-04-30 12:01:26 -0300

    feat: add list modules in sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,5 +1,16 @@
 import { Link } from '@inertiajs/react';
-import { BookOpen, FolderGit2, LayoutGrid } from 'lucide-react';
+import {
+    BookOpen,
+    Calendar,
+    Camera,
+    DollarSign,
+    FolderGit2,
+    Grid2X2,
+    LayoutGrid,
+    Settings,
+    User,
+    Video,
+} from 'lucide-react';
 import AppLogo from '@/components/app-logo';
 import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
@@ -21,6 +32,36 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         href: dashboard(),
         icon: LayoutGrid,
+    },
+    {
+        title: 'Agenda',
+        href: '#',
+        icon: Calendar,
+    },
+    {
+        title: 'Financeiro',
+        href: '#',
+        icon: DollarSign,
+    },
+    {
+        title: 'Câmeras',
+        href: '#',
+        icon: Video,
+    },
+    {
+        title: 'Atletas',
+        href: '#',
+        icon: User,
+    },
+    {
+        title: 'Quadras',
+        href: '#',
+        icon: Grid2X2,
+    },
+    {
+        title: 'Configurações',
+        href: '#',
+        icon: Settings,
     },
 ];
 
